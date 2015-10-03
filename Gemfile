f04932cf1e39b67386f350d3038ffd761de1f4f4
Chester John <chesterjohn@Chesters-MacBook-Pro.local>
2015-10-03 16:58:23 +0800

    layout bootstrap

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'bootstrap-sass'
+gem 'sprockets'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
@@ -46,11 +49,16 @@ group :development, :test do
   gem 'spring'
 
   gem 'rspec-rails'
+  gem 'guard-rspec'
+  gem 'spork-rails'
+  gem 'guard-spork'
+  gem 'childprocess'
 end
 
 group :test do
   gem 'selenium-webdriver'
   gem 'capybara'
+  gem 'growl'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,43 +1,29 @@
 require 'rails_helper'
+require 'spec_helper'
 
 describe "Static pages" do
+
+  subject { page }
+
   describe "Home page" do
-  	it "should have the content 'Sample App'" do
-  	  visit '/static_pages/home'
-  	  expect(page).to have_content('Sample App')
-  	end
+  	before { visit root_url }
 
-  	it "should have the right title, 'Home'" do
-  	  visit 'static_pages/home'
-  	  expect(page).to have_title("Sample App | Home")
-    end
-
+  	it { should have_content('Sample App') }
+  	it { should have_title(full_title('Home')) }
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
-  	  visit '/static_pages/help'
-  	  expect(page).to have_content('Help')
-  	end
+  	before { visit help_path }
 
-  	it "should have the right title, 'Help'" do
-  	  visit 'static_pages/help'
-  	  expect(page).to have_title("Sample App | Help")
-    end
-
+  	it { should have_content('Help') }
+  	it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-  	it "should have the content 'About Us'" do
-  	  visit '/static_pages/about'
-  	  expect(page).to have_content('About Us')
-  	end
+  	before { visit about_path }
 
-  	it "should have the right title, 'About Us'" do
-  	  visit 'static_pages/about'
-  	  expect(page).to have_title("Sample App | About Us")
-    end
-
+  	it { should have_content('About') }
+  	it { should have_title(full_title('About Us')) }
   end
 
 end
